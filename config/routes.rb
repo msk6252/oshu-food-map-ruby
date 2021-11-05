09bfb2419acbbc71929616d1b0f868d096274fc6
msk6252 <msk6252@yahoo.co.jp>
2021-11-05 22:48:55 +0900

    add: リダイレクトと/admin/shopを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admin
   root 'shops#index'
+  get '/admin' => redirect('/admin/sign_in')
+
+  namespace :admin do
+    resources :shops
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

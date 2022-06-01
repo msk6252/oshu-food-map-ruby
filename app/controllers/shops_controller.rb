ea055b108ca933e9843be50991e646bfb84dc4af
msk6252 <msk6252@yahoo.co.jp>
2022-06-02 01:08:15 +0900

    ページネーションの変更

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,11 +1,14 @@
 class ShopsController < ApplicationController
   before_action :set_shop, only: %i[ show edit update destroy ]
 
-  DEFAULT_PAGE = 3
+  DEFAULT_PAGE = 5
 
   # GET /shops or /shops.json
   def index
-    @shops = Shop.all.page(params[:page]).per(DEFAULT_PAGE)
+    @newer = Shop.all.page(params[:page]).per(DEFAULT_PAGE)
+    @nearby = Shop.all.page(params[:page]).per(DEFAULT_PAGE)
+    @anxious = Shop.all.page(params[:page]).per(DEFAULT_PAGE)
+    # @shops = Shop.all
   end
 
   # GET /shops/1 or /shops/1.json

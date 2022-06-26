3ad070f83f49968ddf038bd7667f9a3c05ea6a75
msk6252 <msk6252@yahoo.co.jp>
2022-06-27 00:58:15 +0900

    詳細ページの実装

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -1,2 +1,9 @@
 module ShopsHelper
+  def opening_text(shop)
+    return "一時閉店" if shop.business_status == 2
+    return "閉店" if shop.business_status == 3
+    current_time = Time.now
+    bh = shop.business_hour.where("? between started_at and finished_at", current_time)
+    return  bh.present? ? "営業中" : "準備中"
+  end
 end

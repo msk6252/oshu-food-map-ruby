f603e39a3e38783b94ecaa2c89439ec8e2d0c4a6
msk6252 <msk6252@yahoo.co.jp>
2022-08-20 20:09:00 +0900

    エリア投入スクリプトを作成

--- db/script/import_area.rb
+++ db/script/import_area.rb
@@ -7,8 +7,9 @@ CSV.foreach("db/import/areas.csv") do |row|
   next if row.include?("id")
   area = Area.find_or_initialize_by(id: row[0])
   area.id = row[0]
-  area.city_id = row[1]
-  area.name= row[2]
+  area.name = row[1]
+  area.latitude = row[2]
+  area.longitude= row[3]
   area.save
 end
 
